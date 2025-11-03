c351d411f2d2ba1d9f9644c64c705ebbc0eecbb7
fsubgh <ernestidrisov957@gmail.com>
2025-11-03 06:57:12 -0800

    Add files via upload

---
+++ homework_3.py
@@ -0,0 +1,19 @@
+#Task 1
+#m: int = int(input())
+#n: int = int(input())
+#for i in range(1, m+1):
+ #   if i % 17 == 0 or i % 10 == 9 or (i % 3 and i % 5 == 0):
+  #      print(i)
+
+#Task 2
+#n = int(input())
+#for i in range(1,n+1):
+ #   if (5 <= i <= 9) or (17 <= i <= 37) or (78 <= i <= 87):
+  #      continue
+   # print(i)
+
+#Task 3
+#n: int = int(input())
+#if n <= 9:
+   # for i in range(n):
+    #    print(n, n, n)
