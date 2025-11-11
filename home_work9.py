7b18a73e6745a5487837a06003342f8cefeacefe
fsubgh <ernestidrisov957@gmail.com>
2025-11-11 09:58:42 -0800

    Add files via upload

---
+++ home_work9.py
@@ -0,0 +1,15 @@
+from datetime import date
+today = date.today()
+my_birh_day = date(today.year + 1, 10, 3)
+print(f"До моего дня рождения {(my_birh_day - today).days} дней!")
+
+
+
+import math
+n = 25
+print(f"Квадратный корень: {n}: {math.sqrt(n)}")
+print(f"Факториал из 5: {math.factorial(5)}")
+print(f"Значение пи: {math.pi}")
+print(f"Значение е: {math.e} ")
+
+
