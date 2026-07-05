9c232e81ee76d5aa868f673924a18728ef632043
fsubgh <ernestidrisov957@gmail.com>
2026-07-05 19:25:34 +0300

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import ResizableBox from "./ResizableBox";
+
+function App() {
+  return (
+    <div>
+      <h1>useEffect и useLayoutEffect</h1>
+      <ResizableBox />
+    </div>
+  );
+}
+
+export default App;
