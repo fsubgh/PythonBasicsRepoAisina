12453e3980f771f41c4170a6f48d0af9578b9b09
fsubgh <ernestidrisov957@gmail.com>
2026-07-05 21:41:41 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,10 +1,39 @@
-import TodoList from "./TodoList";
+import "./App.css";
+
+let clickCount = 0;
+
+function handleClick() {
+  clickCount++;
+  console.log("Нажатий:", clickCount);
+}
+
+function handleChange(event) {
+  console.log("Имя:", event.target.value);
+}
+
+function handleMouseOver() {
+  console.log("Мышь наведена!");
+}
 
 function App() {
   return (
-    <div>
-      <h1>Список задач</h1>
-      <TodoList />
+    <div className="App">
+      {/* 1. Кнопка-счетчик */}
+      <button onClick={handleClick}>Нажми меня</button>
+      <p>Количество нажатий смотрите в консоли</p>
+
+      {/* 2. Поле ввода */}
+      <input
+        type="text"
+        placeholder="Введите имя"
+        onChange={handleChange}
+      />
+      <p>Смотрите имя в консоли</p>
+
+      {/* 3. Наведение мышки */}
+      <div className="highlight" onMouseOver={handleMouseOver}>
+        Наведи на меня
+      </div>
     </div>
   );
 }
